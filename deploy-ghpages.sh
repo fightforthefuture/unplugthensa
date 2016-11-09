f2fd0a7b73b8b65dfaa6e7f07a14cb9c473239ca
Jeff Lyon <jeff@rubbingalcoholic.com>
2016-11-09 18:28:49 +0000

    fix deploy script

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd dist || exit 0;
 
-if  [ "$TRAVIS" = "true" -a "$TRAVIS_PULL_REQUEST" = "false"  -a "$TRAVIS_BRANCH" = "production" ]
+if  [ "$TRAVIS" = "true" -a "$TRAVIS_PULL_REQUEST" = "false"  -a "$TRAVIS_BRANCH" = "master" ]
   then
     (cp ../CNAME ./CNAME
      git init
